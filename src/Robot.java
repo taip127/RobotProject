ed35676d900343accab99b8f357355154d2928db
Tai Pham <taipham1192@gmail.com>
2024-11-14 09:54:05 -0600

    finish code

--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Robot {
+    int batteryLevel = 100;
+    int speed = 0;
+    int weight = 50;
+    String color = "red";
+    String name = "Robo";
+    public void displayInfo(){
+        System.out.println(name + " has color: " + color + " and it is weight :"+weight+" lbs");
+    }
+    public void move(){
+        speed = 5;
+        System.out.println(name + "is moving at speed: " + speed + "mph");
+//        System.out.println("%s is moving at speed: %i mph",name,speed);
+    }
+    public void stop(){
+        speed = 0;
+        System.out.println(name + " has stopped");
+    }
+    public void chargeBattery(){
+        batteryLevel = 100;
+        System.out.println(name + " 's battery is fully charged");
+    }
+    public void checkBatteryLevel(){
+        System.out.println(name + "'s current battery level is "+ batteryLevel +"%");
+    }
+
+
+
 }
